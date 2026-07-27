96d5169ee200b3ef5d9f7f6c15b164f8fc4ab1e0
Blancadum <blancadum@gmail.com>
2026-07-27 17:02:22 +0200

    Fix?

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -1,13 +1,40 @@
 import { useEffect, useRef } from 'react';
+import hljs from 'highlight.js/lib/core';
+import bash from 'highlight.js/lib/languages/bash';
+import dockerfile from 'highlight.js/lib/languages/dockerfile';
+import groovy from 'highlight.js/lib/languages/groovy';
+import ini from 'highlight.js/lib/languages/ini';
+import java from 'highlight.js/lib/languages/java';
+import javascript from 'highlight.js/lib/languages/javascript';
+import json from 'highlight.js/lib/languages/json';
+import plaintext from 'highlight.js/lib/languages/plaintext';
+import properties from 'highlight.js/lib/languages/properties';
+import sql from 'highlight.js/lib/languages/sql';
+import xml from 'highlight.js/lib/languages/xml';
+import yaml from 'highlight.js/lib/languages/yaml';
 import { useIsDark } from '../hooks/useIsDark';
 import './CodeBlock.css';
+
+hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('dockerfile', dockerfile);
+hljs.registerLanguage('groovy', groovy);
+hljs.registerLanguage('hcl', ini);
+hljs.registerLanguage('java', java);
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('jsx', javascript);
+hljs.registerLanguage('json', json);
+hljs.registerLanguage('properties', properties);
+hljs.registerLanguage('sql', sql);
+hljs.registerLanguage('text', plaintext);
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('yaml', yaml);
 
 export function CodeBlock({ code, language = 'bash', title = null }) {
   const codeRef = useRef(null);
   const isDark = useIsDark();
 
   useEffect(() => {
-    if (codeRef.current && typeof window !== 'undefined' && window.hljs) {
+    if (codeRef.current) {
       const codeElement = codeRef.current.querySelector('code');
       if (codeElement) {
         codeElement.className = `language-${language} hljs`;
@@ -15,7 +42,7 @@ export function CodeBlock({ code, language = 'bash', title = null }) {
 
         // Force rehighlight even if already highlighted
         delete codeElement.dataset.highlighted;
-        window.hljs.highlightElement(codeElement);
+        hljs.highlightElement(codeElement);
       }
     }
   }, [code, language]);
